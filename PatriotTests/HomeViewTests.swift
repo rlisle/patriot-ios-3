d2d392be5fa49e2aef3868eb0e0ea8fd46bce0b3
Ron <ron@lisles.net>
2023-05-11 08:01:19 -0500

    wip

--- PatriotTests/HomeViewTests.swift
+++ PatriotTests/HomeViewTests.swift
@@ -5,7 +5,7 @@
 //  Created by Ron Lisle on 5/31/22.
 //
 
-
+import Foundation
 import XCTest
 import SwiftUI
 @testable import Patriot3
@@ -13,6 +13,18 @@ import SnapshotTesting
 
 @MainActor
 class HomeViewTests: XCTestCase {
+    
+    func testCurrentPath() {
+        let fileMgr = FileManager()
+        let currentPath = fileMgr.currentDirectoryPath
+        let expectedPath = "/" //"Volumes/workspace/repository"
+        XCTAssertEqual(currentPath, expectedPath)
+    }
+
+   func testIsCI() {
+        let isCI = ProcessInfo.processInfo.environment["CI"] == "TRUE"
+        XCTAssertTrue(isCI)
+    }
     
     func testHomeView() throws {
         let ciPath: StaticString = "/Volumes/workspace/repository/ci_scripts/PatriotTests/HomeViewtests.swift"

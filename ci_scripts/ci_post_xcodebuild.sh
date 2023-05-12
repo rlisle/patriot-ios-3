8af25e37aa87ff9317be167267d7906a01ce8a62
Ron <ron@lisles.net>
2023-05-12 05:21:01 -0500

    add prints to debug

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -6,20 +6,20 @@ pwd
 echo "CI = $CI" # CI = TRUE
 echo "ls -al" # ., .., Artifacts, ci_post_clone.sh, ci_post_xcodebuild.sh
 ls -al
-#echo "ls -al Snapshots"
-#ls -al Snapshots
-#echo "ls -al Snapshots/PatriotTests"
-#ls -al Snapshots/PatriotTests
-echo "ls -al Artifacts/PatriotTests/__Snapshots__/MenuTests" # No such file or directory
-ls -al Snapshots/PatriotTests/__Snapshots__/MenuTests
-echo "ls -al /Volumes/workspace/repository" # ., .., ci_scripts
-ls -al /Volumes/workspace/repository
+echo "ls -al __Snapshots__"
+ls -al __Snapshots__
+echo "ls -al __Snapshots__/PatriotTests"
+ls -al __Snapshots__/PatriotTests
+#echo "ls -al Artifacts/PatriotTests/__Snapshots__/MenuTests" # No such file or directory
+#ls -al Snapshots/PatriotTests/__Snapshots__/MenuTests
+#echo "ls -al /Volumes/workspace/repository" # ., .., ci_scripts
+#ls -al /Volumes/workspace/repository
 #echo "ls -al /Volumes/workspace/repository/iOS"
 #ls -al /Volumes/workspace/repository/iOS
 echo "ls -al /Volumes/workspace/repository/ci_scripts" # ., .., Artifacts, ci_post_clone.sh, ci_post_xcodebuild.sh
 ls -al /Volumes/workspace/repository/ci_scripts
-echo "ls -al Artifacts/PatriotTests/__Snapshots__/MenuTests" # testMenuView.1.png
-ls -al Artifacts/PatriotTests/__Snapshots__/MenuTests
+#echo "ls -al Artifacts/PatriotTests/__Snapshots__/MenuTests" # testMenuView.1.png
+#ls -al Artifacts/PatriotTests/__Snapshots__/MenuTests
 echo "env"
 env
 echo "Done"
